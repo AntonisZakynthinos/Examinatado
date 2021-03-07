e9524f7f95151ec0610c8f70c908425b9c3c147c
AntonisZakynthinos <antzak1998@gmail.com>
2021-03-07 23:21:39 +0200

    last commit

--- app/src/main/java/com/example/examinatado/ReportsActivity.kt
+++ app/src/main/java/com/example/examinatado/ReportsActivity.kt
@@ -3,11 +3,11 @@ package com.example.examinatado
 
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.ImageButton
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 
 class ReportsActivity : AppCompatActivity() {
+    lateinit var listview: ListView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_reports)
@@ -19,5 +19,16 @@ class ReportsActivity : AppCompatActivity() {
             val i = Intent(applicationContext, ProfileActivity::class.java)
             startActivity(i)
         }
+        listview = findViewById(R.id.listview) as ListView
+        val arrayList:ArrayList<String> = ArrayList()
+
+        arrayList.add("10/9/2017")
+        arrayList.add("9/6/2017")
+        arrayList.add("23/2/2017")
+        arrayList.add("3/12/2016")
+        arrayList.add("6/8/2016")
+
+        var arrayAdapter = ArrayAdapter(this, android.R.layout.simple_list_item_1, arrayList)
+        listview.setAdapter(arrayAdapter)
     }
 }
